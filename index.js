a06c2ea4c1c971b45513f9ccd43304302b42bca5
Testimony <e.romero@bees2biz.com>
2022-01-18 12:41:09 -0500

    Erikson004-SocketIO initial configuration

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Express initialization
 const express = require("express");
 const app = express();
+// Module for paths in express
+const path = require('path');
 // Routes lecture
 const routes = require("./config/routes");
 
@@ -10,7 +12,22 @@ routes(app);
 // Port declaration
 app.set('port', process.env.PORT || 3000);
 
+// Static files on server
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Server Port
-app.listen(app.get('port'), function() {
-    console.log('Server');
+const server = app.listen(app.get('port'), function() {
+    console.log(`Server running on port: ${app.get('port')}`);
+});
+
+// Declaration of Socket IO
+const socketIo = require("socket.io");
+// Creation of the Socket IO Server
+const io = socketIo(server);
+
+// Websockets
+
+// Cpnnection of a new User
+io.on('connection', () => {
+    console.log(`New Connection`);
 });
